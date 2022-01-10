8a0c7771218b046fcde0c80e98e90001e5d494b0
Marlene Moraes <marlenevmoraes@outlook.com>
2022-01-10 18:16:23 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,7 @@
+function Form() {
+    return (
+        <h1>Página de formulário</h1>
+    );
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+function Listing() {
+    return (
+        <h1>Página de listagem</h1>
+    );
+}
+
+export default Listing;
