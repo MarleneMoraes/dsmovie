a6b7f16943ad675088329c9e490ddd96498aa56f
Marlene Moraes <marlenevmoraes@outlook.com>
2022-01-10 18:08:08 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+
+
 function App() {
   return (
-    <h1>Página principal</h1>
+    <Navbar />
   );
 }
 
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import './styles.css';
+
+function Navbar() {
+    return (
+      <header>
+        <nav className="container">
+          <div className="dsmovie-nav-content">
+            <h1>DSMovie</h1>
+            <a href="https://github.com/MarleneMoraes" target="_blank" rel="noreferrer">
+              <div className="dsmovie-contact-container">
+                <GithubIcon />
+                <p className="dsmovie-contact-link">/devsuperior</p>
+              </div>
+            </a>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+
+export default Navbar;
