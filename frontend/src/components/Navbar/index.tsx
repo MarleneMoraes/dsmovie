12eead0ba294c8cb3913f992d4cd2571cf49028e
Marlene Moraes <marlenevmoraes@outlook.com>
2022-01-14 12:12:48 -0300

    Show score

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -10,7 +10,7 @@ function Navbar() {
             <a href="https://github.com/MarleneMoraes" target="_blank" rel="noreferrer">
               <div className="dsmovie-contact-container">
                 <GithubIcon />
-                <p className="dsmovie-contact-link">/devsuperior</p>
+                <p className="dsmovie-contact-link">/MarleneMoraes</p>
               </div>
             </a>
           </div>
