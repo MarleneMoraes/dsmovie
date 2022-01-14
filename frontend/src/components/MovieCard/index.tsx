2e1545dfc6199d2b625de106982fc57f6ce88e51
Marlene Moraes <marlenevmoraes@outlook.com>
2022-01-14 11:25:21 -0300

    Props

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,14 +1,13 @@
-import { Link } from "react-router-dom";
 import MovieScore from 'components/MovieScore';
+import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
 
-function MovieCard() {
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+type Props = {
+    movie: Movie;
+}
+
+
+function MovieCard( { movie } : Props) {
 
     return (
         <div>
