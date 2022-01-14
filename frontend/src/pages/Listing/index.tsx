0c41b50aa8a9a728c7e98c4d433fd338eb587aab
Marlene Moraes <marlenevmoraes@outlook.com>
2022-01-14 12:25:07 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -26,12 +26,15 @@ function Listing() {
                 const data = response.data as MoviePage;
                 setPage(data);
             });
-    }, []);
+    }, [pageNumber]);
 
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
 
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange}/>
             <div className="container">
                 <div className="row">
                     {page.content.map(movie =>    (
